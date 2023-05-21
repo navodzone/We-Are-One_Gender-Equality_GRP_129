8beefdbf3d6d00a46a31f9fe85a304d2af9d1114
Prashanth-fdo <132833632+Prashanth-fdo@users.noreply.github.com>
2023-05-21 22:28:47 +0530

    Final project ITPM

---
+++ IT19967462/database/migrations/2023_05_20_105214_create_healths_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHealthsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('healths', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('image');
+            $table->string('vlink');
+            $table->text('description');
+            $table->string('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('healths');
+    }
+}
